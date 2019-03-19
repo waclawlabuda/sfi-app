7fd34fec477957ce4aa2ac7a025d8fd964295cfa
x-kom Arkadiusz Czekajski <arkadiusz.czekajski@x-kom.org>
2019-03-19 22:48:21 +0100

    arek thingz

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import JestSample from './JestSample/JestSample';
-import EnzymeSample from './EnzymeSample/EnzymeSample';
-import TestComponent from './EnzymeSample/Challenge/TestComponent';
+import Page from './Page/Page';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <EnzymeSample onSubmit={() => console.log('submitted!')} />
-          <TestComponent
-            name={'John Doe'}
-            age={'36'}
-            checked={true}
-          />
+          <Page />
         </header>
       </div>
     );
