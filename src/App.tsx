817fff8d1b50c64041bf69562685beee92643ea4
Arkadiusz Czekajski <aczekajski@gmail.com>
2019-03-20 14:00:05 +0100

    rzeczy do drugiej części warsztatu (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import JestSample from './JestSample/JestSample';
-import EnzymeSample from './EnzymeSample/EnzymeSample';
-import TestComponent from './EnzymeSample/Challenge/TestComponent';
+import Page from './Page/Page';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <JestSample />
-          {/* <EnzymeSample onSubmit={() => console.log('submitted!')} /> */}
-          {/* <TestComponent
-            name={'John Doe'}
-            age={'36'}
-            checked={true}
+          <Page />
+          {/* <JestSample /> */}
+          {/* <EnzymeSample onSubmit={() => console.log('submitted!')} /> */}	
+          {/* <TestComponent	
+            name={'John Doe'}	
+            age={'36'}	
+            checked={true}	
           /> */}
         </header>
       </div>
