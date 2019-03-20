26ce43c240676874df2510b652ff600223e07738
x-kom Arkadiusz Czekajski <arkadiusz.czekajski@x-kom.org>
2019-03-20 18:38:49 +0100

    single candidate route

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import EnzymeSample from '../EnzymeSample/EnzymeSample';
+import EnzymeSample, { EnzymeSampleState } from '../EnzymeSample/EnzymeSample';
 import TestComponent from '../EnzymeSample/Challenge/TestComponent';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
@@ -13,6 +13,44 @@ interface PageState {
 
 const API = 'http://localhost:3030';
 
+type CandidateState = Pick<EnzymeSampleState, 'name' | 'age' | 'checked'>;
+
+class Candidate extends React.Component<{ id: number }, CandidateState> {
+    state: CandidateState = {
+        age: '',
+        name: '',
+        checked: false,
+    };
+
+    componentDidMount() {
+        this.getCandidate(this.props.id);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.id !== this.props.id) {
+            this.getCandidate(this.props.id);
+        }
+    }
+
+    getCandidate = async (id: number) => {
+        try {
+            const res = await fetch(`${API}/candidates/${id}`);
+            const data = await res.json();
+            this.setState({ ...data });
+        } catch (e) {
+            console.error('Nie pobrano');
+        }
+    }
+
+    render() {
+        return (<TestComponent
+            name={this.state.name}
+            age={this.state.age}
+            checked={this.state.checked}
+        />);
+    }
+}
+
 class Page extends React.Component<PageProps, PageState> {
     onAddFactory = (history) => async (data) => {
         try {
@@ -25,7 +63,7 @@ class Page extends React.Component<PageProps, PageState> {
             });
             const entry = await res.json();
             history.push(`/candidate/${entry.id}`);
-        } catch(e) {
+        } catch (e) {
             console.error('Submitting failed ;(');
         }
     }
@@ -42,11 +80,7 @@ class Page extends React.Component<PageProps, PageState> {
                 )}
                 />
                 <Route path={'/candidate/:id'} render={({ match }) => (
-                    <TestComponent
-                        name={'John Doe'}
-                        age={'36'}
-                        checked={true}
-                    />
+                    <Candidate id={match.params.id} />
                 )} />
                 <Route path={'/'} exact={true} render={({ match }) => (
                     'Everything!'
